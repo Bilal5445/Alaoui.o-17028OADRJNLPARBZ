7561fb79a062d709d9dba42033916f74d6df8084
namatedev <namatedev@localhost>
2017-08-08 11:53:42 +0100

    MrArbz>Corpus : autofill using Twingly : full loop > lm creation : speed up

--- RUN_transl_srilm.sh
+++ RUN_transl_srilm.sh
@@ -1,1 +1,1 @@
-srilm-1.7.2/bin/cygwin64/ngram-count -text corpus/170426_extended_dict.txt -order 3 -lm lm/moroccan_arabic_corpus_01.lm
+srilm-1.7.2/bin/cygwin64/ngram-count -text corpus/170426_extended_dict.txt -order 3 -lm lm/moroccan_arabic_corpus_01.lm -write-binary-lm
