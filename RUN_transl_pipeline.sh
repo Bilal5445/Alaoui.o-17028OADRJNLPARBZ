85c1b0e4b3e1be34b9bf023301e6691b7a9d50b7
namatedev <namatedev@localhost>
2017-11-23 12:52:06 +0000

    arabiziweb>perf>train : speed up perl module : rewriting script

--- RUN_transl_pipeline.sh
+++ RUN_transl_pipeline.sh
@@ -3,15 +3,11 @@ TEST=example/small-example # Set this to the filename to transliterate (the inpu
 BASEDIR=.
 ARABIC_LM=lm/moroccan_arabic_corpus_01.lm
 DISAMBIG=srilm-1.7.2/bin/cygwin64/disambig.exe
-# WPAIRS_DATA_FILE_ARABIZI=/path/to/arabizi/side/of/bitext # (e.g. LDC2013E125)
 WPAIRS_DATA_FILE_ARABIZI=arabizi-arabic-bitext/arabizi-arabic-bitext.arz
-# WPAIRS_DATA_FILE_ARABIC=/path/to/arabic/side/of/bitext # (e.g. LDC2013E125)
 WPAIRS_DATA_FILE_ARABIC=arabizi-arabic-bitext/arabizi-arabic-bitext.ar
-#####
 
-# ADD_WORD_PAIRS=1   # Set this to 1 to add transliterated word pairs, otherwise 0
-ADD_WORD_PAIRS=0
-NB_CORES=1         # If processing a large file, increase this to parallelize tokenization step
+# NB_CORES=1         # If processing a large file, increase this to parallelize tokenization step
+NB_CORES=2
 
 # scripts and binaries
 TOKENIZE_EN=$BASEDIR/scripts/tokenizeE.pl
@@ -29,60 +25,46 @@ WSUFF=""
 WPAIRS_DATA_AZ=""
 WPAIRS_DATA_AR=""
 
-if [ $ADD_WORD_PAIRS == 1 ]; then
-    WSUFF="+word"
-    
-    cat $WPAIRS_DATA_FILE_ARABIZI| perl $TOKENIZE_EN - - | perl $LOWERCASE > $WPAIRS_DATA_FILE_ARABIZI.ENTOK.lc
-    cat $WPAIRS_DATA_FILE_ARABIC | perl $TOKENIZE_EN - - | perl $LOWERCASE > $WPAIRS_DATA_FILE_ARABIC.ENTOK.lc
-    
-    WPAIRS_DATA_AZ=$WPAIRS_DATA_FILE_ARABIZI.ENTOK.lc
-    WPAIRS_DATA_AR=$WPAIRS_DATA_FILE_ARABIC.ENTOK.lc
-fi
+timestamp() {
+  #date +"%s"
+  date +%H:%M:%S:%3N
+}
 
 ######### PIPELINE STARTS HERE ############
 
+# Phase I : tokenize, lowercase, get mapping variants
 if [ 1 == 1 ]; then
+    timestamp 
     echo "# tokenize and lowercase the Arabizi file" 
     cat $TEST.arabizi | perl $TOKENIZE_EN - - | perl $LOWERCASE > $TEST.1.arabizi.ENTOK.lc
 
+    timestamp
     echo "# extract dictionary of Arabizi test"
     cat $TEST.1.arabizi.ENTOK.lc | perl $TRAINDICT > $TEST.2.arabizi.ENTOK.lc.dict
 
+    timestamp
     echo "# get possible Arabizi->Arabic mappings"
     ( perl $TRANSCM --use-lm=no $ARABIC_DICT < $TEST.2.arabizi.ENTOK.lc.dict > $TEST.3.lc.az-ar.lex ) >& $TEST.4.lc.az-ar.lex.log
-fi
-
-if [ $ADD_WORD_PAIRS == 1 ]; then
-    echo "# extract word transliteration pairs from Arabizi-Arabic corpus (tritext)"
-    ( perl $EXTRACTWPAIRS \
-        --arabizi=$WPAIRS_DATA_AZ \
-        --arabic=$WPAIRS_DATA_AR \
-        --map=$TEST.3.lc.az-ar.lex > $TEST.3.lc.az-ar.lex$WSUFF ) \
-        >& $TEST.4.lc.az-ar.lex$WSUFF.log
 fi
 
 if [ 1 == 1 ]; then
 
     #
+    timestamp
     echo "# call SRILM disambig with LM"
     echo "$DISAMBIG -keep-unk -map $MAP $TEST.3.lc.az-ar.lex$WSUFF -text $TEST.1.arabizi.ENTOK.lc -lm $ARABIC_LM -order $LMORDER | sed -r 's/^<s> //;s/ <\/s>$//' > $TEST.5.arabizi.ENTOK.lc.disambig$WSUFF"
     $DISAMBIG -keep-unk \
         -map $MAP $TEST.3.lc.az-ar.lex$WSUFF -text $TEST.1.arabizi.ENTOK.lc \
         -lm $ARABIC_LM -order $LMORDER \
-        -mapw 100 \
         | sed -r 's/^<s> //;s/ <\/s>$//' \
     > $TEST.5.arabizi.ENTOK.lc.disambig$WSUFF
 
+    timestamp
     echo "# post-processing for special characters, smileys etc."
     $POSTPROCESS < $TEST.5.arabizi.ENTOK.lc.disambig$WSUFF > $TEST.6.arabizi.ENTOK.lc.disambig$WSUFF.pp
 fi
 
+timestamp
 echo "The final transliterated file is here:"
-
-if [ $ADD_WORD_PAIRS == 1 ]; then
-    cp $TEST.6.arabizi.ENTOK.lc.disambig$WSUFF.pp $TEST.7.charWordTransl
-    echo $TEST.7.charWordTransl
-else
-    cp $TEST.6.arabizi.ENTOK.lc.disambig$WSUFF.pp $TEST.7.charTransl
-    echo $TEST.7.charTransl
-fi
+cp $TEST.6.arabizi.ENTOK.lc.disambig$WSUFF.pp $TEST.7.charTransl
+echo $TEST.7.charTransl
